5758b111c7d5cc7797fe2a4606bb7b48a3a032c9
tapos ghosh <tapos.aa@gmail.com>
2017-11-15 05:15:44 +0600

    registration full complete and login page markup and basic functionality done

--- public/js/tutexp_sms_wordpress_login-public.js
+++ public/js/tutexp_sms_wordpress_login-public.js
@@ -1,8 +1,66 @@
+AccountKit_OnInteractive = function(){
+    AccountKit.init(
+        {
+            appId:"1952653494947783",
+            state:randomString(20),
+            version:"v1.0",
+            fbAppEventsEnabled:true,
+            redirect:"http://my-wp.dev/wp-admin/admin-ajax.php?action=tutexpFacebookDataFetch1"
+        }
+    );
+};
+function randomString(length) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
 (function ($) {
     'use strict';
 
+    var mobileNumber = null;
+    var countryCode = null;
+
+    function loginCallback(response) {
+
+        if (response.status === "PARTIALLY_AUTHENTICATED") {
+            var code = response.code;
+            var csrf = response.state;
+            jQuery.ajax({
+                url : tutexp_ajax.ajax_url,
+                type : 'post',
+                data : {
+                    'action' : 'tutexpFacebookDataFetch',
+                    'code' : code,
+                    'csrf' : csrf,
+                    'countryCode':countryCode,
+                    'mobileNumber':mobileNumber
+                },
+                success : function( response ) {
+                    console.log(response);
+                    var  str = response.substring(0, response.length - 1);
+                    window.location = str;
+                }
+            });
+
+            // Send code to server to exchange for access token
+        }
+        else if (response.status === "NOT_AUTHENTICATED") {
+            // handle authentication failure
+        }
+        else if (response.status === "BAD_PARAMS") {
+            // handle bad parameters
+        }
+    }
+
+    // phone form submission handler
+    function smsLogin(countryCode,phoneNumber) {
+        // debugger;
+        AccountKit.login(
+            'PHONE',
+            {countryCode: countryCode, phoneNumber: phoneNumber}, // will use default values if not specified
+            loginCallback
+        );
+    }
     // $("#mobile-number").intlTelInput();
-    $("#mobile-number").intlTelInput({}).done(function () {
+    $("#mobile-number,#country").intlTelInput({}).done(function () {
         // analytics
         $('.selected-flag').one('click', function () {
             ga('send', 'event', 'demo', 'clicked selected country');
@@ -29,6 +87,74 @@
         }
 
     });
+
+
+
+    $('.tutexpForm .form').find('input, textarea').on('keyup blur focus', function (e) {
+
+        var $this = $(this),
+            label = $this.prev('label');
+
+        if (e.type === 'keyup') {
+            if ($this.val() === '') {
+                label.removeClass('active highlight');
+            } else {
+                label.addClass('active highlight');
+            }
+        } else if (e.type === 'blur') {
+            if ($this.val() === '') {
+                label.removeClass('active highlight');
+            } else {
+                label.removeClass('highlight');
+            }
+        } else if (e.type === 'focus') {
+
+            if ($this.val() === '') {
+                label.removeClass('highlight');
+            }
+            else if ($this.val() !== '') {
+                label.addClass('highlight');
+            }
+        }
+
+    });
+
+    $('.tutexpForm .tab a').on('click', function (e) {
+
+        e.preventDefault();
+
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+
+        var target = $(this).attr('href');
+
+        $('.tutexpForm .tab-content > div').not(target).hide();
+
+        $(target).fadeIn(600);
+
+    });
+
+    $('.smsLogin').on('click',function (e) {
+
+        var isValid = $("#mobile-number").intlTelInput("isValidNumber");
+        if(!isValid){
+            e.preventDefault();
+            alert("phone number not valid");
+        }else{
+
+            var intlNumber = $("#mobile-number").intlTelInput("getNumber");
+
+
+
+        }
+
+        e.preventDefault();
+       // smsLogin(countryCode,mobileNumber);
+    });
+
+    $('.emailLogin').on('click',function (e) {
+        alert("email click");
+    })
 
 
 
--- templates/login_form.php
+++ templates/login_form.php
@@ -12,17 +12,46 @@
             ?>
         </p>
     <?php endif; ?>
-    <?php
-    wp_login_form(
-        array(
-            'label_username' => __( 'Email', 'personalize-login' ),
-            'label_log_in' => __( 'Sign In', 'personalize-login' ),
-            'redirect' => $attributes['redirect'],
-        )
-    );
-    ?>
+    <div class="tutexpForm">
+        <div class="form">
+            <ul class="tab-group mytabls">
+                <li class="tab active"><a href="#signup">Phone</a></li>
+                <li class="tab"><a href="#login">Email</a></li>
+            </ul>
+            <form method="post" id="form">
+                <?php wp_nonce_field('state', 'state'); ?>
+            </form>
+            <div class="tab-content">
+                <div id="signup">
+                    <h1>Account Kit</h1>
+                    <div>
+                        <p class="form-row">
 
-    <a class="forgot-password" href="<?php echo wp_lostpassword_url(); ?>">
-        <?php _e( 'Forgot your password?', 'personalize-login' ); ?>
-    </a>
+                            <label for="mobile_number"><?php _e('Mobile Number', 'personalize-login'); ?></label>
+                            <input style="color: white !important;background-color: green !important; " type="tel" name="mobile_number" id="mobile-number">
+                        </p>
+                        <button class="button button-block smsLogin"/>
+                        Send</button>
+                    </div>
+                </div>
+                <div id="login">
+                    <h1>Account Kit</h1>
+                    <div>
+                        <div class="field-wrap">
+                            <label>
+                                Email Address<span class="req">*</span>
+                            </label>
+                            <input type="email" id="email" required autocomplete="off"/>
+                        </div>
+                        <button type="submit" class="button button-block emailLogin"/>
+                        Log In</button>
+                    </div>
+                </div>
+            </div>
+            <!-- tab-content -->
+        </div>
+        <!-- /form -->
+    </div>
+
+
 </div>
